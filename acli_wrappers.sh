46eb96513418853656c873e28d1e62137d622f7f
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-12-01 10:21:45 +0200

    [.^] [Compile] Removed quoting around board name variable..
    It does not contain spaces. Quotes worsen readability.

--- acli_wrappers.sh
+++ acli_wrappers.sh
@@ -3,7 +3,7 @@
 # Wrapped Arduino CLI commands
 
 # Author: Martin Eden
-# Last mod.: 2025-11-29
+# Last mod.: 2025-12-01
 
 #
 # Return arduino-cli board name for Arduino Uno
@@ -46,7 +46,7 @@ uno_compile() {
       arduino-cli \
         compile \
         . \
-        --fqbn "$board_name" \
+        --fqbn $board_name \
         --clean \
         --quiet \
         --warnings all \
